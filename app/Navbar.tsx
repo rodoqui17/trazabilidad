a2b2162393b777d76194f7283fd9f588bc6214cb
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2024-09-24 19:49:06 -0400

    update panel component

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,7 +2,6 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Image from "react-bootstrap/Image";
 function Navigation({ user, onLogout }) {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
