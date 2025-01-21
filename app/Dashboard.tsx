96ea298e9198c1668daddafae0d6250ac43dde96
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2025-01-21 00:46:08 -0400

    update

--- app/Dashboard.tsx
+++ app/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { User } from "firebase/auth";
 import Navigation from "./Navbar";
 import TrazabilidadMinerales from "./components/panel";
