56528343b652740ea19d9db2927796f28efa6b27
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2025-01-21 01:17:59 -0400

    update

--- app/Authentication.tsx
+++ app/Authentication.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, User } from "firebase/auth";
 import UserPanel from "./Dashboard"; // Asegúrate de que la ruta sea correcta
-import imagen1 from "./img/imagen1.webp";
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
