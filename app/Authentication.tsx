7742989d29d10382487dd55e100685da6cdcbcd3
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2025-01-21 02:23:45 -0400

    update

--- app/Authentication.tsx
+++ app/Authentication.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, User } from "firebase/auth";
 import UserPanel from "./Dashboard"; // Asegúrate de que la ruta sea correcta
-import Carousel from "./Carousel";
+// import Carousel from "./Carousel";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -68,7 +68,7 @@ const LandingPage = () => {
 
   {/* Hero Section */}
  
-<Carousel/>
+{/* <Carousel/> */}
   {/* Features Section */}
   <section id="features" className="py-20 bg-black text-white">
   <div className="container mx-auto text-center">
