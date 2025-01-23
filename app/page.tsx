0426ab9bddd2b5519e3cf26fc3cb6d01c4ed61c8
Rodrigo Quiroz <quiroz.rodrigo@gmail.com>
2025-01-23 11:48:14 -0400

    update site

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
 "use client";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Authentication from "./Authentication";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function Home() {
   return (
-    <Router>
-      <main>
-        <Routes>
-          <Route path="/" element={<Authentication />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
-    </Router>
+    <div>
+      {<Authentication />}
+    </div>
+    // <Router>
+    //   <main>
+    //     <Routes>
+    //       <Route path="/" element={<Authentication />} />
+    //       <Route path="/about" element={<About />} />
+    //       <Route path="/contact" element={<Contact />} />
+    //     </Routes>
+    //   </main>
+    // </Router>
   );
 }
 
